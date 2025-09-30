3f2fd740810c0e569a32264391fa4ed07b4012ed
Greon68 <greon-ya@yandex.ru>
2025-09-30 20:44:09 +0300

    initial

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+// TODO: write code here
+
+// comment this to pass build
+const unusedVariable = "variable";
+
+// for demonstration purpose only
+export default function demo(value) {
+  return `Demo: ${value}`;
+}
+
+console.log("app.js included");
+
+import { func } from "./example";
+
+const container = document.querySelector(".container");
+func(container);
